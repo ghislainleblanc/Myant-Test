c51f94ed4c808776e76896438b8dbbd29dba0924
Ghislain Leblanc <ghislain.leblanc@clevertech.biz>
2020-09-09 11:10:23 -0400

    Get location.

--- Myant Test/Myant Test/ViewController.swift
+++ Myant Test/Myant Test/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
+        getCurrentLocation()
+    }
+
+    private func getCurrentLocation() {
+        let locManager = CLLocationManager()
+        locManager.requestWhenInUseAuthorization()
+        locManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locManager.delegate = self
+        locManager.requestLocation()
+    }
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            return
+        }
+
+        print("Lat: \(location.coordinate.latitude) Long: \(location.coordinate.longitude)")
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
