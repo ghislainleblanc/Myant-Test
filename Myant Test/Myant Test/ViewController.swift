b02e87788235897c2fb11ba963d9cc5264d31b3b
Ghislain Leblanc <ghislain.leblanc@clevertech.biz>
2020-09-09 10:12:04 -0400

    Empty project.

---
+++ Myant Test/Myant Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Myant Test
+//
+//  Created by Ghislain Leblanc on 2020-09-09.
+//  Copyright © 2020 Leblanc, Ghislain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
